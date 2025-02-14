18c58dcf4a0a211db6f60761b9ea13c050051a4b
phK3 <ufedz@student.kit.edu>
2025-02-14 10:26:46 +0100

    execute network with arbitrary input vector datatype

--- src/NNLoader/Layers_Vector.jl
+++ src/NNLoader/Layers_Vector.jl
@@ -1,14 +1,14 @@
-function (N::Network)(x :: Vector{Float64})
+function (N::Network)(x :: AbstractVector)
     for L in N.layers
         x = L(x)
     end
     return x
 end
 
-function (L::Dense)(x :: Vector{Float64})
+function (L::Dense)(x :: AbstractVector)
     return L.W * x .+ L.b
 end
 
-function (L::ReLU)(x :: Vector{Float64})
+function (L::ReLU)(x :: AbstractVector)
     return max.(x,0.0)
 end
